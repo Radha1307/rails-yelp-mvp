7a241c275e7f21e80354951a8ec5f0cf031256f5
Radha Patel <radha13.patel@gmail.com>
2021-05-26 17:06:43 +0100

    got my first restaurant app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Restaurant.destroy_all
+puts 'seeding 5 restaurants each with between 1-5 reviews...'
+5.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.street_address
+  phone_number = Faker::PhoneNumber.cell_phone
+  category = %w[chinese italian japanese french belgian].sample
+  restaurant = Restaurant.create!(name: name, address: address, phone_number: phone_number, category: category)
+
+  rand(1..5).times do
+    Review.create!(rating: rand(0..5), restaurant: restaurant, content: Faker::Lorem.paragraph)
+  end
+end
+puts 'seeding complete'
